50fa5aa13b048fb2f55c8339989b2a3e9a29867c
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-10-17 10:20:18 +0200

    Creado metodo cambiarColor()

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -2,27 +2,36 @@ package com.example.simondice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-
-        val btnStart : Button = findViewById(R.id.btn_start)
-
-    }
-    override fun onResume() {
-        super.onResume()
         val btnRojo : Button = findViewById(R.id.button_red)
         val btnVerde : Button = findViewById(R.id.button_green)
         val btnAmarillo : Button = findViewById(R.id.button_yellow)
         val btnAzul : Button = findViewById(R.id.button_blue)
+        val btnStart : Button = findViewById(R.id.btn_start)
 
+        btnStart.setOnClickListener { view ->
+            btnStart.visibility = View.INVISIBLE
+            cambiarColor(btnRojo, btnVerde, btnAmarillo, btnAzul)
+        }
     }
 
+    private fun cambiarColor(rojoBtn : Button, verdeBtn : Button, amarilloBtn : Button, azulBtn : Button) {
+        when (genColor()) {
+            R.color.rojo_encendido -> rojoBtn.setBackgroundResource(R.color.rojo_encendido)
+            R.color.verde_encendido -> verdeBtn.setBackgroundResource(R.color.verde_encendido)
+            R.color.amarillo_encendido -> amarilloBtn.setBackgroundResource(R.color.amarillo_encendido)
+            R.color.azul_encendido -> azulBtn.setBackgroundResource(R.color.azul_encendido)
+        }
+    }
     private fun genColor(): Int {
         val color = when (Random().nextInt(4) + 1) {
             1 -> R.color.rojo_encendido
