6e0438371718a2857d5b7a5d0c377aecc577368d
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-11-28 14:06:55 +0100

    Empezando a implementar el acceso mediante Google Login One Tap

--- app/src/main/java/com/example/simondice/LoginActivity.kt
+++ app/src/main/java/com/example/simondice/LoginActivity.kt
@@ -1,11 +1,94 @@
 package com.example.simondice
 
+import android.content.Intent
+import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+    private val TAG = "OneTap:"
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+    private var showOneTapUI = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        // CONFIGURACIÓN CLIENTE DE ACCESO ONE TAP
+        oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            // para el acceso con Google
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // ID de la app que agregamos al proyecto (esta app)
+                    .setServerClientId("1086336482884-kehqrq7u9nhu6ljtv5c0to2u2rensnjk.apps.googleusercontent.com")
+                    // esto hace que el cliente OneTap sólo solicite a los usuarios que accedan
+                    // con cuentas Google que ya hayan utilizado previamente
+                    .setFilterByAuthorizedAccounts(false)
+                    .build())
+            .build()
+
+        // MOSTRAR IU DE ACCESO ONE TAP
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(this) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0)
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow, or
+                // do nothing and continue presenting the signed-out UI.
+                e.localizedMessage?.let { Log.d(TAG, "FailureListener: "+it) }
+            }
+
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    val username = credential.id
+                    val password = credential.password
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with your backend.
+                            Log.d(TAG, "Got ID token.")
+                        }
+                        password != null -> {
+                            // Got a saved username and password. Use them to authenticate
+                            // with your backend.
+                            Log.d(TAG, "Got password.")
+                        }
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d(TAG, "No ID token or password!")
+                        }
+                    }
+                } catch (e: ApiException) {
+                    // ...
+                }
+            }
+        }
+    }
+
 }
